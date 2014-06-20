11527c23ef06eb0278e11b36c93f11c7d804dd14
Joe Hobson <joseph.j.hobson@googlemail.com>
2014-06-20 11:09:57 +0100

    Initial commits

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="css/wind.css">
+  <title>Wind Power Supply</title>
+</head>
+ <body>
+  <ul></ul>
+  <button></button>
+  <div class="turbine">
+    <div class="rotor">
+      <div class="rotor-blade blade1"></div>
+      <div class="rotor-blade blade2"></div>
+      <div class="rotor-blade blade3"></div>
+      <div class="rotor-tip"></div>
+    </div>
+  <div class="mast"></div>
+</div>
+</body>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+</html>
+
+
+
---
+++ js/script.js
@@ -0,0 +1,13 @@
+$('button').click(function () {
+  $.getJSON( "grid_watch_a.json", function(obj) {
+
+    $.each(obj, function(key, value){
+      $("ul").append("<li>" + "Demand for power is " + value.demand + "GW" + "</li>"),
+      $("ul").append("<li>" + "Wind Power supplies " + value.wind + "GW" + "</li>"),
+      $("ul").append("<li>" + "Wind Power accounts for around " + ((value.wind / value.demand) * 100).toFixed(2) + "%" + " of the total Energy Supply" + "</li>")
+    });
+  });
+});
+
+
+
