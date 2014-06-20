38bc724805f24a764884324030b6f4f5ba032e28
Joe Hobson <joseph.j.hobson@googlemail.com>
2014-06-20 15:05:10 +0100

    Attaching JS to data

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $('button').one( "click", function() {
   $.getJSON( "../power/json/grid_watch_a.json", function(obj) {
+
     $.each(obj, function(key, value){
       $("ul").append("<li>" + "Demand for power is " + value.demand + "GW" + "</li>"),
       $("ul").append("<li>" + "Wind Power supplies " + value.wind + "GW" + "</li>"),
@@ -8,5 +9,17 @@ $('button').one( "click", function() {
   });
 });
 
+$(document).ready(function() {
 
+  var wind = 10; // need this to be the output of our JSON func, ((value.wind / value.demand) * 100).toFixed(2)
 
+      if (wind <= 1) {
+        $('.rotor').css('-webkit-animation', 'rotate 30s infinite linear');
+      }
+      else if (wind > 1 && wind <= 2) {
+        $('.rotor').css('-webkit-animation', 'rotate 15s infinite linear');
+      }
+      else {
+        $('.rotor').css('-webkit-animation', 'rotate 3s infinite linear');
+      }
+});
