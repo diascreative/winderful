6a926df42ec3d02cbcb6d20bc939f76001eb6243
Joe Hobson <joseph.j.hobson@googlemail.com>
2014-06-20 16:34:57 +0100

    Links css animation to json data

--- js/script.js
+++ js/script.js
@@ -1,17 +1,11 @@
 $('button').one( "click", function() {
   $.getJSON( "./json/grid_watch_a.json", function(obj) {
+    var value = obj[0];
+      $("ul").append("<li>" + "Demand for power is " + value.demand + "GW" + "</li>");
+      $("ul").append("<li>" + "Wind Power supplies " + value.wind + "GW" + "</li>");
+      $("ul").append("<li>" + "Wind Power accounts for around " + ((value.wind / value.demand) * 100).toFixed(2) + "%" + " of the total Energy Supply" + "</li>");
 
-    $.each(obj, function(key, value){
-      $("ul").append("<li>" + "Demand for power is " + value.demand + "GW" + "</li>"),
-      $("ul").append("<li>" + "Wind Power supplies " + value.wind + "GW" + "</li>"),
-      $("ul").append("<li>" + "Wind Power accounts for around " + ((value.wind / value.demand) * 100).toFixed(2) + "%" + " of the total Energy Supply" + "</li>")
-    });
-  });
-});
-
-$(document).ready(function() {
-
-  var wind = 10; // need this to be the output of our JSON func, ((value.wind / value.demand) * 100).toFixed(2)
+      var wind = ((value.wind / value.demand) * 100).toFixed(2); // need this to be the output of our JSON func, ((value.wind / value.demand) * 100).toFixed(2)
 
       if (wind <= 1) {
         $('.rotor').css('-webkit-animation', 'rotate 30s infinite linear');
@@ -22,4 +16,7 @@ $(document).ready(function() {
       else {
         $('.rotor').css('-webkit-animation', 'rotate 3s infinite linear');
       }
+  });
 });
+
+
