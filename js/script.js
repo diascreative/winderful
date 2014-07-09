467d5df5fef2fdb9979302b632097892f8302fdf
pire <rob.mccormack@diascreative.com>
2014-07-09 13:44:18 +0200

    correct indentation

--- js/script.js
+++ js/script.js
@@ -2,34 +2,35 @@ $('form').one("submit", function(event) {
   event.preventDefault();
 
   var send = $(this).serialize()
-    $.ajax({
-      type: 'GET',
-      url: './download.php',
-      data: send,
-      dataType: 'json',
-      success: function(obj) {
-        var value = obj[0];
 
-          $("ul").append("<li>" + "Demand for power is " + value.demand + "GW" + "</li>");
-          $("ul").append("<li>" + "Wind Power supplies " + value.wind + "GW" + "</li>");
-          $("ul").append("<li>" + "Wind Power accounts for around " + ((value.wind / value.demand) * 100).toFixed(2) + "%" + " of the total Energy Supply" + "</li>");
+  $.ajax({
+    type: 'GET',
+    url: './download.php',
+    data: send,
+    dataType: 'json',
+    success: function(obj) {
+      var value = obj[0];
 
-          var wind = ((value.wind / value.demand) * 100).toFixed(2); // need this to be the output of our JSON func, ((value.wind / value.demand) * 100).toFixed(2)
+        $("ul").append("<li>" + "Demand for power is " + value.demand + "GW" + "</li>");
+        $("ul").append("<li>" + "Wind Power supplies " + value.wind + "GW" + "</li>");
+        $("ul").append("<li>" + "Wind Power accounts for around " + ((value.wind / value.demand) * 100).toFixed(2) + "%" + " of the total Energy Supply" + "</li>");
 
-          if (wind <= 3) {
-            $('.rotor').css('-webkit-animation', 'rotate 40s infinite linear');
-          }
-          else if (wind > 3 && wind <= 8) {
-            $('.rotor').css('-webkit-animation', 'rotate 20s infinite linear');
-          }
-          else {
-            $('.rotor').css('-webkit-animation', 'rotate 2s infinite linear');
-          }
-        },
-      error: function() {
-        alert('Error');
-      }
-    });
+        var wind = ((value.wind / value.demand) * 100).toFixed(2); // need this to be the output of our JSON func, ((value.wind / value.demand) * 100).toFixed(2)
+
+        if (wind <= 3) {
+          $('.rotor').css('-webkit-animation', 'rotate 40s infinite linear');
+        }
+        else if (wind > 3 && wind <= 8) {
+          $('.rotor').css('-webkit-animation', 'rotate 20s infinite linear');
+        }
+        else {
+          $('.rotor').css('-webkit-animation', 'rotate 2s infinite linear');
+        }
+      },
+    error: function() {
+      alert('Error');
+    }
+  });
 });
 
 $('#datetimepicker_start').datetimepicker({
