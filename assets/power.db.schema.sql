0bd1753c1cd21b7aeea0212bf40937edc6e2381b
pire <pire@pire.local>
2014-07-21 17:37:59 +0200

    remove auto increment from SQL

--- assets/power.db.schema.sql
+++ assets/power.db.schema.sql
@@ -28,7 +28,7 @@ CREATE TABLE `wind_vs_demand` (
   `demand` int(8) DEFAULT NULL,
   `wind` int(8) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=330616 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
