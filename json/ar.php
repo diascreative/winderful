864d57ac60b8e9e88955067a2890f15e1e2bbf28
pire <pire@pire.local>
2014-08-11 17:49:56 +0200

    ardiuno return only wind number :D

---
+++ json/ar.php
@@ -0,0 +1,16 @@
+<?php
+  error_reporting(0);
+
+  require_once( dirname(__FILE__) . '/../inc/init.php' );
+
+  $query = fetchAssoc(query("SELECT
+              timestamp,
+              demand,
+              wind
+            FROM
+              wind_vs_demand
+            ORDER BY
+              timestamp
+            LIMIT 1"));
+
+  echo $query['wind'];
