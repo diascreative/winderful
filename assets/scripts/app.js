d1faa99796202455401feca330b475cd2865285f
pire <rob.cmm@gmail.com>
2014-10-15 15:41:10 +0200

    scope changes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,12 +3,14 @@
 
   var app = angular.module('windTurbineApp', ['ngBootstrap', 'angular-rickshaw']);
 
-  app.controller('GraphController', ['$scope', function($scope) {
-    $scope.options = {
+  app.controller('turbineController', function() {
+    this.graph = {};
+
+    this.graph.options = {
       renderer: 'area'
     };
 
-    $scope.series = [{
+    this.graph.series = [{
       name: 'Series 1',
       color: 'steelblue',
       data: [{x: 0, y: 23}, {x: 1, y: 15}, {x: 2, y: 79}, {x: 3, y: 31}, {x: 4, y: 60}]
@@ -17,6 +19,6 @@
       color: 'lightblue',
       data: [{x: 0, y: 30}, {x: 1, y: 20}, {x: 2, y: 64}, {x: 3, y: 50}, {x: 4, y: 15}]
     }];
-  }]);
+  });
 
 })(angular);
