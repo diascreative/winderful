1a213041d0725f97418abbf3a2bd81e29cfa907a
pire <rob.cmm@gmail.com>
2014-10-27 11:17:22 +0100

    improve speed turbine

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -59,7 +59,7 @@
       var $rContainer = $('#turbine-rotor-container');
       var $rotor = $('#turbine-rotor').eq(0);
 
-      var valueS = (11 - speed),
+      var valueS = (13 - speed),
         value = valueS + 's',
         degPerS = 360/(valueS*40),
         oldTransform = matrixToDeg($rotor.css('transform')),
